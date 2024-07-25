78363777946779934de826a1fee1c8343ce37cbc
i-AryanGupta <aryangupta6008@gmail.com>
2024-07-25 16:19:44 +0530

    pom gitigone src file added

---
+++ src/main/java/com/cam/ClassroomAssignementManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomAssignementManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomAssignementManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cam/ClassroomAssignementManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomAssignementManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
